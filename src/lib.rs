d799115b2423b3058d6ec36b4430ab82bfefe2ea
Akira Kano <a.kano1101@gmail.com>
2023-05-20 11:39:27 +0900

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use jsonwebkey_convert::{Base64BigUint, JsonWebKey, JsonWebKeySet};
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+
+#[derive(Debug, serde::Deserialize)]
+struct TokenClaims {
+    #[allow(dead_code)]
+    sub: String,
+}
+
+#[allow(dead_code)]
+fn to_base64url<F>(rsa_key: &JsonWebKey, f: F) -> anyhow::Result<String>
+where
+    F: FnOnce(&jsonwebkey_convert::RSAPublicKey) -> &Base64BigUint,
+{
+    let rsa_public_key = rsa_key
+        .rsa_public_key()
+        .ok_or(anyhow::anyhow!("failed to get rsa public key"))?;
+
+    let public_key_string = f(rsa_public_key).to_base64url();
+
+    Ok(public_key_string)
+}
+
+#[allow(dead_code)]
+async fn decode_username_from_token(token: &str, jwks_url: &str) -> anyhow::Result<String> {
+    let kid = jsonwebtoken::decode_header(token)?
+        .kid
+        .ok_or(anyhow::anyhow!("failed to decoding header."))?;
+
+    let response = reqwest::get(jwks_url).await?;
+
+    let jwks: JsonWebKeySet = response.json().await?;
+
+    let rsa_key: JsonWebKey = jwks
+        .keys
+        .into_iter()
+        .find(|key| match &key {
+            JsonWebKey::RSAPublicKey { value, .. } => value.generic.kid.as_ref() == Some(&kid),
+            _ => false,
+        })
+        .ok_or(anyhow::anyhow!("failed to matching generic kid."))?;
+
+    // 適切な方法で適切なキーを選択する
+    let modulus = &to_base64url(&rsa_key, |key| &key.n)?;
+    let exponent = &to_base64url(&rsa_key, |key| &key.e)?;
+
+    // RSA公開鍵を作成し、デコーディングキーを生成
+    let decoding_key = DecodingKey::from_rsa_components(modulus, exponent)?;
+
+    // デコード
+    let validation = Validation::new(Algorithm::RS256);
+    let validated_token = decode::<TokenClaims>(token, &decoding_key, &validation)?;
+
+    let claims = validated_token.claims;
+    let sub = claims.sub;
+
+    Ok(sub)
+}
